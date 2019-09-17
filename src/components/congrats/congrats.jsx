540e094f14646620039ac06cc06de4b7d3e21d27
Sally Beckwith <sallypbeckwith@gmail.com>
2019-09-17 14:27:48 -0400

    Extract enzyme setupt to setuptTests.js

--- src/components/congrats/congrats.jsx
+++ src/components/congrats/congrats.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Congrats = (props) => {
   const { success } = props;
@@ -12,5 +13,8 @@ const Congrats = (props) => {
   )
 }
 
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired,
+}
 
 export default Congrats;
