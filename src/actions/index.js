80f43146937a6e014f609dfb13ef1afd072d1b10
Sally Beckwith <sallypbeckwith@gmail.com>
2019-09-19 10:00:13 -0400

    added integration tests for guessed word and secret word

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { getLetterMatchCount } from '../helpers';
+
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
@@ -5,6 +7,21 @@ export const actionTypes = {
 
 export const guessWord = (guessedWord) => {
   return (dispatch, getState) => {
+    const secretWord = getState().secretWord; //getState returns an object, so we get secretWord property from it
+    const letterMatchCount = getLetterMatchCount(secretWord, guessedWord);
 
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: {
+        guessedWord,
+        letterMatchCount
+      }
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({
+        type: actionTypes.CORRECT_GUESS
+      })
+    }
   };
 }
